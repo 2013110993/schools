c439bb213b2707c928fd371d8388e5ae8ae48f9c
federico rosado <2013110993@ub.edu.bz>
2022-09-15 01:44:35 -0600

    create a writeJSON helper that the handler will call to write the responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -17,21 +16,12 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-	//Convert our map into a JSON object
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encontered a problem and could process your request", http.StatusInternalServerError)
 		return
 	}
-	//Add a newline to make viewing on the terminal easier
-	js = append(js, '\n')
-	//Specify that we will server our responses using JSON
-	w.Header().Set("Content-Type", "application/json")
-
-	//Write the []byte slice containing the JSON response body
-	w.Write(js)
-
 	//Write the JSON as the HTTP response body
 	//w.Write([]byte(js))
 
