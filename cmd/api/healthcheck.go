f45a028fe1224f7924f04f4f73f2553a88c4cdb3
federico rosado <2013110993@ub.edu.bz>
2022-09-15 00:52:11 -0600

    fix createSchoolHandler and showSchoolHandler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,8 @@ import (
 	"net/http"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	//	js := `{"status": "available", "environment": %q, "version": %q }`
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -4,14 +4,16 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
-func (app *application) routes() *httprouter.Router  {
+
+func (app *application) routes() *httprouter.Router {
 	//Create a new httprouter router instance
 	router := httprouter.New()
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck",app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/schools",app.createSchoolHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/schools/:id",app.showSchoolHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/schools", app.createSchoolHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/schools/:id", app.showSchoolHandler)
 
 	return router
 }
