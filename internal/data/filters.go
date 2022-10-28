d08ca6b88d6571a9ff23d78519c29b3ead9cf02d
federico rosado <2013110993@ub.edu.bz>
2022-10-28 13:40:01 -0600

    added metadata field to our response

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -2,7 +2,12 @@
 
 package data
 
-import "schools.federicorosado.net/internal/validator"
+import (
+	"math"
+	"strings"
+
+	"schools.federicorosado.net/internal/validator"
+)
 
 type Filters struct {
 	Page     int
@@ -19,4 +24,55 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be maximum of 100")
 	// Check that the sort parameter matches a value in the acceptable sort list
 	v.Check(validator.In(f.Sort, f.SortList...), "sort", "invalid sort value")
+}
+
+// The sortColmn() method safety extracts the sort field query parameter
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// The sortOrder() method determins whether we should sort by DESC/ASC
+func (f Filters) sortOrder() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+// The limit() method determins the LIMIT
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+// The offset() method calculates the OFFSET
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
+// The Metadata type contains metadata to help with pagination
+type Metadata struct {
+	CurrentPage  int `json: "current_page, omitempty"`
+	PageSize     int `json: "page_size, omitempty"`
+	FirstPage    int `json: "first_page, omitempty"`
+	LastPage     int `json: "last_page, omitempty"`
+	TotalRecords int `json: "total_records, omitempty"`
+}
+
+// The calculateMetadata() functions computes the values For the Metada fields
+func calculateMetadata(totalRecords int, page int, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
--- internal/data/schools.go
+++ internal/data/schools.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -208,26 +209,29 @@ func (m SchoolModel) Delete(id int64) error {
 }
 
 // the GetAll() method returns a list of all the shcools sorted by id
-func (m SchoolModel) GetAll(name string, level string, mode []string, filers Filters) ([]*School, error) {
+func (m SchoolModel) GetAll(name string, level string, mode []string, filters Filters) ([]*School, Metadata, error) {
 	// Construct the query
-	query := `
-		SELECT id, created_at, name, level, contact, phone, email, website, address, mode, version
+	query := fmt.Sprintf(`
+		SELECT COUNT(*) OVER(), id, created_at, name, level, contact, phone, email, website, address, mode, version
 		FROM schools
 		WHERE (to_tsvector('simple', name) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (to_tsvector('simple', level) @@ plainto_tsquery('simple', $2) OR $2 = '')
 		AND (mode @> $3 OR $3 = '{}' )
-		ORDER BY id
-	`
+		ORDER BY %s %s, id ASC
+		LIMIT $4 OFFSET $5`, filters.sortColumn(), filters.sortOrder())
+
 	//Create a 3-second-timeout context
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+	args := []interface{}{name, level, pq.Array(mode), filters.limit(), filters.offset()}
 	//Execute the query
-	rows, err := m.DB.QueryContext(ctx, query, name, level, pq.Array(mode))
+	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 	//Close the resultset
 	defer rows.Close()
+	totalRecords := 0
 	// Initialize an empty slide to hold the School data
 	schools := []*School{}
 	// Iterate over the rows in the result set
@@ -235,6 +239,7 @@ func (m SchoolModel) GetAll(name string, level string, mode []string, filers Fil
 		var school School
 		// Scan the values from the row into the School
 		err := rows.Scan(
+			&totalRecords,
 			&school.ID,
 			&school.CreatedAt,
 			&school.Name,
@@ -248,15 +253,16 @@ func (m SchoolModel) GetAll(name string, level string, mode []string, filers Fil
 			&school.Version,
 		)
 		if err != nil {
-			return nil, err
+			return nil, Metadata{}, err
 		}
 		// Add the school tour slice
 		schools = append(schools, &school)
 	}
 	// Check for errors after looping through the resultset
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
 	// Return the slice of schools
-	return schools, nil
+	return schools, metadata, nil
 }
